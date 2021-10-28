f193dee3e5b17a85ee23063a21f0fef86f1bedf0
Sakshi <saks5011@gmail.com>
2021-10-28 19:22:03 +0530

    notesApp

---
+++ app.js
@@ -0,0 +1,142 @@
+const textArea = document.querySelector("#add-text");
+const addButton = document.querySelector("#add-btn");
+const searchTxt = document.querySelector("#search-txt")
+showNotes()
+
+// adding notes
+function addNote() {
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = []
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    let text = textArea.value;
+    notesObj.push(text);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    textArea.value = ""
+
+    showNotes();
+}
+
+// deleting a note
+
+function deleteNote(index){
+    console.log(index);
+    let notes = localStorage.getItem("notes");
+      if (notes == null) {
+          notesObj = []
+      }
+      else {
+          notesObj = JSON.parse(notes);
+      }
+  
+      notesObj.splice(index,1);
+      localStorage.setItem("notes",JSON.stringify(notesObj))
+      showNotes();
+  }
+  
+
+
+//  showing notes
+
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = []
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+
+    let html = "";
+    notesObj.forEach((element, index) => {
+        html += `<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Note ${index + 1}</h5>
+          <p class="card-text">${element}</p>
+          <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-outline-danger">Delete Note</button>
+        </div>
+      </div>`
+    });
+
+
+    let notesELement = document.getElementById("notes");
+    if (notesObj.length != 0)
+        notesELement.innerHTML = html;
+    else
+     notesELement.innerHTML=`Nothing to show! Use "Add note" section to add some notes!!`
+}
+
+function searchItems(e){
+  let inputVal = (e.target.value.toLowerCase());
+  let noteCards = document.getElementsByClassName("noteCard");
+  Array.from(noteCards).forEach((element)=>{
+      let cardTxt = element.getElementsByTagName("p")[0].innerText
+      if(cardTxt.includes(inputVal))
+        element.style.display = "block"
+    else
+        element.style.display = "none"
+    //   console.log(cardTxt)
+  })
+}
+
+
+// event listeners
+
+searchTxt.addEventListener("input",(e)=>searchItems(e));
+addButton.addEventListener("click", () => addNote())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let newNote = document.createElement("div")
+//     newNote.setAttribute("class","noteCard my-2 mx-2 card")
+//     newNote.setAttribute("id",`note${count}`);
+//     newNote.setAttribute("style","width: 18rem;")
+
+//     let insideNewNote = document.createElement("div");
+//     insideNewNote.setAttribute("class","card-body")
+//     newNote.appendChild(insideNewNote);
+
+//     let noteHeading = document.createElement("h5");
+//     noteHeading.setAttribute("class","card-title")
+//     insideNewNote.appendChild(noteHeading);
+//     noteHeading.innerText="Note";
+
+//     let noteText = document.createElement("p");
+//     noteText.setAttribute("class","card-text")
+//     insideNewNote.appendChild(noteText);
+//     let newNoteText = document.createTextNode(text);
+//     noteText.appendChild(newNoteText);
+
+//     let addNoteBUtton = document.createElement("button");
+//     addNoteBUtton.setAttribute("class","btn btn-outline-danger")
+//     addNoteBUtton.setAttribute("id",`btn${count}`);
+//     insideNewNote.appendChild(addNoteBUtton);
+//     addNoteBUtton.innerText="Delete Note";
+
+
+//     notesSection.appendChild(newNote)
+//     // console.log(textArea)
+
+//     count++;
+
+// console.log(addNoteBUtton, newNote)
+
